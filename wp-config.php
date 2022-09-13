013fb3102084c7dbd3dddc20950500623fc91b6e
elancesoft <elancefoxvn@gmail.com>
2022-09-13 11:20:00 +0700

    update code from live

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'elancefoxvn_bmw' );
+define( 'DB_NAME', 'elancefoxvn_porsche' );
 
 /** MySQL database username */
 define( 'DB_USER', 'elancefoxvn_dev' );
